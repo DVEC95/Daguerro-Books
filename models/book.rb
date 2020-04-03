09c645698466ffd92c6be481b1b1b19c1242a2e1
David Eastcroft <davideastcroft95@gmail.com>
2020-04-03 19:21:01 +0100

    Adds save, update, delete and delete all methods.

--- models/book.rb
+++ models/book.rb
@@ -17,4 +17,29 @@ class Book
     @id = book['id'].to_i if book['id']
   end
 
+  def save()
+    sql = "INSERT INTO books (title, synopsis, format, genre, cost, price, quantity, author_id)
+          VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id"
+    values = [@title, @synopsis, @format, @genre, @cost, @price, @quantity, @author_id]
+    @id = SQLRunner.run(sql, values)[0]['id'].to_i
+  end
+
+  def update()
+    sql = "UPDATE books SET (title, synopsis, format, genre, cost, price, quantity, author_id)
+          = ($1, $2, $3, $4, $5, $6, $7, $8) WHERE id = $9"
+    values = [@title, @synopsis, @format, @genre, @cost, @price, @quantity, @author_id, @id]
+    SQLRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM books WHERE id = $1"
+    values = [@id]
+    SQLRunner.run(sql, values)
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM books"
+    SQLRunner.run(sql)
+  end
+
 end
