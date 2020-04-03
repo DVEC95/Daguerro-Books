1c5ba2fc404d616dfca342959d22708daa4a8a25
David Eastcroft <davideastcroft95@gmail.com>
2020-04-03 17:38:53 +0100

    Creates tables for books and authors.

---
+++ db/bookshop.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS authors;
+
+CREATE TABLE authors(
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  active BOOLEAN
+);
+
+CREATE TABLE books(
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  synopsis VARCHAR(500),
+  format VARCHAR(255),
+  genre VARCHAR(255),
+  cost DECIMAL(10, 2),
+  price DECIMAL(10, 2),
+  quantity INT
+);
