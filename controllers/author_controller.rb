5f75d811a914a2c9efecd8a0925f44f57bc250a8
David Eastcroft <davideastcroft95@gmail.com>
2020-04-05 17:18:18 +0100

    Adds RESTful navigation.

---
+++ controllers/author_controller.rb
@@ -0,0 +1,54 @@
+require("sinatra")
+require("sinatra/contrib/all") if development?
+
+require_relative("../models/book.rb")
+require_relative("../models/author.rb")
+
+#INDEX
+get "/authors" do
+  @authors = Author.all()
+  erb(:"authors/index")
+end
+
+#NEW
+get "/authors/new" do
+  erb(:"authors/new")
+end
+
+#SHOW
+get "/authors/:id" do
+  id = params['id'].to_i
+  @author = Author.find(id)
+  erb(:"authors/show")
+end
+
+#CREATE
+post "/authors" do
+  new_author = Author.new(params)
+  new_author.save
+  erb(:"authors/create")
+end
+
+#DELETE
+post "/authors/:id/delete" do
+  id = params['id'].to_i
+  author = Author.find(id)
+  author.delete_books
+  author.delete
+  redirect("/authors")
+end
+
+#EDIT
+get "/authors/:id/edit" do
+  @authors = Author.all()
+  id = params['id'].to_i
+  @author = Author.find(id)
+  erb(:"authors/edit")
+end
+
+#UPDATE
+post "/authors/:id" do
+  author = Author.new(params)
+  author.update()
+  redirect("/authors/" + params["id"])
+end
