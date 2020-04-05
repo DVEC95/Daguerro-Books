a0baf3a1f8d749a219d59da578616f4a868a8df9
David Eastcroft <davideastcroft95@gmail.com>
2020-04-05 17:14:43 +0100

    Adds requirement for author controller.

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require("sinatra")
 require("sinatra/contrib/all") if development?
 
 require_relative("controllers/book_controller")
+require_relative("controllers/author_controller")
 
 get "/" do
   erb(:index)
