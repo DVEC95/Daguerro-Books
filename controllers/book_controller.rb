b714175b2a1a29e1d7ece3bf72a4111daae9190d
David Eastcroft <davideastcroft95@gmail.com>
2020-04-05 15:25:53 +0100

    Adds RESTful routes.

--- controllers/book_controller.rb
+++ controllers/book_controller.rb
@@ -30,10 +30,26 @@ post "/books" do
   erb(:"books/create")
 end
 
+#DELETE
+post "/books/:id/delete" do
+  id = params["id"].to_i
+  book = Book.find(id)
+  book.delete
+  redirect("/books")
+end
+
 #EDIT
 get "/books/:id/edit" do
   @authors = Author.all()
+  @books = Book.all()
   id = params['id'].to_i
   @book = Book.find(id)
   erb(:"books/edit")
+end
+
+#UPDATE
+post "/books/:id" do
+  book = Book.new(params)
+  book.update()
+  redirect("/books/" + params["id"])
 end
