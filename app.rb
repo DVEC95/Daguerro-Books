8711c54680fd7438a997ae066ab9a12f631a0e26
David Eastcroft <davideastcroft95@gmail.com>
2020-04-07 21:10:48 +0100

    Adds requirement for genre controller.

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require("sinatra/contrib/all") if development?
 
 require_relative("controllers/book_controller")
 require_relative("controllers/author_controller")
+require_relative("controllers/genre_controller")
 
 get "/" do
   erb(:index)
