6729e8422a4ba5c13051609a04e12edf60ddb764
David Eastcroft <davideastcroft95@gmail.com>
2020-09-15 21:56:17 +0100

    Adds heroku config

--- app.rb
+++ app.rb
@@ -6,6 +6,8 @@ require_relative("controllers/book_controller")
 require_relative("controllers/author_controller")
 require_relative("controllers/genre_controller")
 
-get "/" do
-  erb(:index)
+class App < Sinatra::Base
+  get "/" do
+    erb(:index)
+  end
 end
